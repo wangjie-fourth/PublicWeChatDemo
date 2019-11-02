3c9b9b8385cb67fe68be84718707109902e85aad
wangjie_fourth <wangjie_fourth@163.com>
2019-11-02 09:20:08 +0800

    修改gitignore文件,添加测试maven插件

---
+++ src/main/java/com/wangjie/github/publicwechatdemo/PublicwechatdemoApplication.java
@@ -0,0 +1,13 @@
+package com.wangjie.github.publicwechatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicwechatdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicwechatdemoApplication.class, args);
+    }
+
+}
