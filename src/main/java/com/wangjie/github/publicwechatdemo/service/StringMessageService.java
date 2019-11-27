2e52fbf47d56000bbbfff27d073a5e2f4ccb4c32
wangjie_fourth <wangjie_fourth@163.com>
2019-11-27 14:47:23 +0800

    fix

--- src/main/java/com/wangjie/github/publicwechatdemo/service/StringMessageService.java
+++
@@ -1,7 +0,0 @@
-package com.wangjie.github.publicwechatdemo.service;
-
-import com.wangjie.github.publicwechatdemo.messagedomain.requset.TextMessageRqeuest;
-
-public interface StringMessageService {
-     String handleStringMessage(TextMessageRqeuest textMessageRqeuest);
-}
